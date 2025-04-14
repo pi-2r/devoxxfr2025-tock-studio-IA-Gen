55fd6420bd2b4ca7c84e742c30af3f35e618821f
Benjamin BERNARD <benjamin.bernard@benvii.com>
2025-04-14 20:10:11 +0200

    Ollama local proxy with bash script to download all images

--- lab-server/store_ollama_images_local_registry.sh
+++ lab-server/store_ollama_images_local_registry.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-# docker run -d -p 5000:5000 --restart always --name registry registry:2
+# Ensure you local ollama model folder is removed so that every layers get pulled !
+# rm -rf ~/.cache/ollama/models
 
 models=(
+    "gemma3:12b"
+    "gemma3:4b"
+    "mistral:7b"
+    "phi4:14b"
+    "qwen2.5:14b"
+    "qwen2.5:7b"
+    "qwen2.5:3b"
+    "nomic-embed-text:latest"
     "qwen2.5:1.5b"
 )
 
 for MODEL in "${models[@]}"; do
+    echo "Pulling model $MODEL from local registry..."
     ollama pull --insecure http://localhost:9200/library/$MODEL
-
 done
 
